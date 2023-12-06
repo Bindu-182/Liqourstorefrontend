23d31b8dc0d2b1bf35ed2c25d98ed0c971d6345b
Sharath <mantripragadasharath@gmail.com>
2023-12-05 20:20:55 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,65 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="center-container">
+      <LiquorList :liquors="liquors" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import LiquorList from "@/components/LiquorList.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    LiquorList,
+  },
+  data() {
+    return {
+      liquors: [],
+    };
+  },
+  mounted() {
+    this.fetchLiquors();
+  },
+  methods: {
+    async fetchLiquors() {
+      try {
+        const response = await axios.get("https://liqourstorebackend.onrender.com/api");
+        this.liquors = response.data;
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+html, body {
+  height: 100%;
+  margin: 0;
 }
+
+#app {
+  font-family: 'Poppins', sans-serif;
+  background-color: #0a1b34;
+  height: 100%;
+  width:100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.center-container {
+  max-width: 800px;
+  width: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+  text-align: center;
+}
+
+
 </style>
---
+++ src/components/LiquorList.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="liquor-store">
+      <h2>Charger's Liquor Store</h2>
+      <div v-for="liquor in liquors" :key="liquor._id" class="liquor-container" @click="showDetails(liquor)">
+        <div class="image-container">
+          <img :src="liquor.image" alt="Liquor Image" class="liquor-image" />
+        </div>
+        <div class="info-container">
+          <h3>{{ liquor.name }}</h3>
+          <p><strong>Type:</strong> {{ liquor.type }}</p>
+          <p><strong>Description:</strong> {{ liquor.description }}</p>
+          <p><strong>Brand:</strong> {{ liquor.brand }}</p>
+          <p><strong>Price:</strong> ${{ liquor.price }}</p>
+          <p><strong>Alcohol Percentage:</strong> {{ liquor.alcoholContent }}%</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['liquors'],
+    methods: {
+      showDetails(liquor) {
+        console.log('Clicked on liquor:', liquor);
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .liquor-store {
+  width: 100%;
+  height: 100%;
+  max-width: 800px;
+  padding: 20px;
+  margin: 0 auto; 
+}
+  
+  .liquor-container {
+    display: flex;
+    border: 1px solid #ddd;
+    margin-bottom: 20px;
+    background-color: #c49c24;
+    cursor: pointer;
+    transition: transform 0.2s ease-in-out;
+    border-radius: 8px;
+    overflow: hidden;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  }
+  
+  .liquor-container:hover {
+    transform: scale(1.02);
+  }
+  
+  .image-container {
+  flex: 0 0 auto;
+  width: 150px; 
+  height: 150px; 
+  border-radius: 8px 0 0 8px;
+  overflow: hidden;
+}
+
+.liquor-image {
+  width: 100%;
+  height: 100%;
+  object-fit: fill;
+  object-position: center; 
+  border-radius: 8px 0 0 8px;
+}
+  
+  .info-container {
+    flex-grow: 1;
+    padding: 20px;
+  }
+  
+  
+  h2, h3, p {
+    color: #41261a;
+    margin: 0px;
+  }
+  
+  h3 {
+    margin-bottom: 10px;
+  }
+  h2{
+    color:#f89429;
+    margin-bottom: 25px;
+  }
+  </style>
+  
